fd3e8549696eb86602b7c0badd7e3c8c596404a5
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2023-03-27 20:41:02 -0300

    initial commit

---
+++ src/main/java/com/oliveira/hexagonal/adapters/out/SendCpfForValidationAdapter.java
@@ -0,0 +1,19 @@
+package com.oliveira.hexagonal.adapters.out;
+
+import com.oliveira.hexagonal.application.ports.out.SendCpfForValidationOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendCpfForValidationAdapter implements SendCpfForValidationOutputPort {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Override
+    public void send(String cpf) {
+        kafkaTemplate.send("tp-cpf-validation", cpf);
+    }
+
+}
---
+++ src/main/java/com/oliveira/hexagonal/application/ports/out/SendCpfForValidationOutputPort.java
@@ -0,0 +1,6 @@
+package com.oliveira.hexagonal.application.ports.out;
+
+public interface SendCpfForValidationOutputPort {
+
+    void send(String cpf);
+}
